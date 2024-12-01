7ba7db475bf7f4c0a767b69a0c9cfd325c4bd5ff
SergeyBildanov <troktrokov@gmail.com>
2024-12-02 00:51:09 +0400

    vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sergeybildanov.github.io'
+  base: '/sergeybildanov.github.io/decomposition'
 })
